098de0842269e356020f1437878e4cda760f1ccd
Ernestover <ernestastover@icloud.com>
2026-04-09 12:16:09 -0500

    started on printintro function

--- calculator/main.c
+++ calculator/main.c
@@ -17,7 +17,7 @@ void doMath(char op);
 
 
 int main() {
-
+    printIntro();
     return 0;
 }
 
@@ -25,20 +25,17 @@ int main() {
  * @brief prints the intro statement of the program 
  */
 void printIntro() {
-
+    printf("Welcome to my Calculator program!!!\n");
+    for(char i=0; i<40;i++) printf("-");
+    printf("\nAllowed operands: +,-,*,/ \n");
+    
 }
 
-/**
- * @brief prints the operators and shows how data is to be inputted
- */
-char showOperators() {
-
-}
 
 /**
  * @brief does the math operation based on the operator 
  * @note returns the result to the terminal 
  */
 void doMath(char op) {
-    
+
 }
